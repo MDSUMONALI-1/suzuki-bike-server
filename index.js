e491d7d83da5d8baaf85440b78b899bcbe6e7430
unknown <mdsumonaliceng@gmail.com>
2021-11-09 23:52:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { MongoClient } = require("mongodb");
+const app = express()
+const port = 5000
+
+const uri =
+  "mongodb+srv://aliceng:CPYGPkH0idj7KPLX@cluster0.aq2lr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('store');
+    const bikes = database.collection('bikes');
+    console.log("database connected")
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { name: 'honda' };
+    const bike = await bikes.findOne(query);
+    console.log(bike);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello assainment World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
