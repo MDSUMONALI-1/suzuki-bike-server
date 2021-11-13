1bd81b011e168f14b2ff0b7357dd50a7e4ef766a
unknown <mdsumonaliceng@gmail.com>
2021-11-13 15:27:10 +0600

    5

--- index.js
+++ index.js
@@ -32,6 +32,43 @@ async function run() {
       res.json(bike);
   })
  
+  app.get('/users/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    let isAdmin = false;
+    if (user?.role === 'admin') {
+        isAdmin = true;
+    }
+    res.json({ admin: isAdmin });
+})
+
+app.post('/users', async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    console.log(result);
+    res.json(result);
+});
+
+app.put('/users', async (req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const options = { upsert: true };
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.json(result);
+});
+
+  app.put('/users/admin', async (req, res) => {
+    const user = req.body;
+    console.log('put',user)
+    const filter = { email: user.email };
+ 
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.json(result);
+});
+
   } finally {
     
     // await client.close();
