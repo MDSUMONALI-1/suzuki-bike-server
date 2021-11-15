2c3c796e5c622f4cbd248b44c435f736c7175831
unknown <mdsumonaliceng@gmail.com>
2021-11-15 07:37:55 +0600

    1

--- index.js
+++ index.js
@@ -39,6 +39,16 @@ async function run() {
     console.log(result);
     res.json(result);
   });
+app.delete('bikes/:id', async (req, res)=>{
+const id = req.params.id;
+const query ={_id: ObjectId(id)}
+const result = await bikesCollection.deleteOne(query);
+res.json(result)
+
+
+})
+
+
   app.get('/users/:email', async (req, res) => {
     const email = req.params.email;
     const query = { email: email };
